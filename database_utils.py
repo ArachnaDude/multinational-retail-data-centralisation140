0d1e7e926c95d580ec10e67668d2a36b59dd9f39
Matt <cunning.distraction@googlemail.com>
2024-06-19 00:56:23 +0100

    complete list_db_tables function

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,5 @@
 import yaml
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 class DatabaseConnector():
 
@@ -9,15 +9,22 @@ class DatabaseConnector():
       print(data)
       return data
   
-  def init_db_engine(self, cred_dict):
+  def init_db_engine(self):
     cred_dict = self.read_db_creds()
     engine = create_engine(f"postgresql+psycopg2://{cred_dict['RDS_USER']}:{cred_dict['RDS_PASSWORD']}@{cred_dict['RDS_HOST']}:{cred_dict['RDS_PORT']}/{cred_dict['RDS_DATABASE']}")
     return engine
   
-  def list_db_tables(self, engine):
+  def list_db_tables(self) -> None:
     engine = self.init_db_engine()
+    engine.execution_options(isolation_level="AUTOCOMMIT").connect()
+    m = MetaData()
+    m.reflect(engine)
+    for table in m.tables.values():
+      print(table.name)
+  
     
 
-connection = DatabaseConnector()
+if __name__ == "__main__":
+  connection = DatabaseConnector()
 
-connection.read_db_creds()
+  connection.list_db_tables()
