9b6195feaca884e53e3ab110c03664507345d5ed
Matt <cunning.distraction@googlemail.com>
2024-06-19 22:26:05 +0100

    complete step 4 functionality

--- database_utils.py
+++ database_utils.py
@@ -6,7 +6,6 @@ class DatabaseConnector():
   def read_db_creds(self):
     with open("./db_creds.yaml", "r") as stream:
       data = yaml.safe_load(stream)
-      print(data)
       return data
   
   def init_db_engine(self):
@@ -14,17 +13,19 @@ class DatabaseConnector():
     engine = create_engine(f"postgresql+psycopg2://{cred_dict['RDS_USER']}:{cred_dict['RDS_PASSWORD']}@{cred_dict['RDS_HOST']}:{cred_dict['RDS_PORT']}/{cred_dict['RDS_DATABASE']}")
     return engine
   
-  def list_db_tables(self) -> None:
+  def list_db_tables(self):
     engine = self.init_db_engine()
     engine.execution_options(isolation_level="AUTOCOMMIT").connect()
-    m = MetaData()
-    m.reflect(engine)
-    for table in m.tables.values():
-      print(table.name)
+    metadata = MetaData()
+    metadata.reflect(engine)
+    print("Tables in this database:")
+    
+    for table in metadata.tables.keys():
+      print(table)
+    return list(metadata.tables.keys())
   
     
 
 if __name__ == "__main__":
   connection = DatabaseConnector()
-
   connection.list_db_tables()
