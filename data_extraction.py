9e942dfc0372e850f4da0bc3962c6983aef71c61
Matt <cunning.distraction@googlemail.com>
2024-06-20 02:56:23 +0100

    adjust function to return dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,23 @@
+from database_utils import DatabaseConnector
+import pandas as pd
+
+
 class DataExtractor():
   
   
-  def read_rds_table(self, engine, table_name):
-    
-    pass
+  def read_rds_table(self, engine, table_name="legacy_users"):
+    engine.connect()
+    df = pd.read_sql_table(table_name, engine)
+    # print(type(df))
+    return df
   
+
+if __name__ == "__main__":
+  connection = DatabaseConnector()
+  engine = connection.init_db_engine()
+  table_list = connection.list_db_tables()
+
+  extractor = DataExtractor()
+
+  for table in table_list:
+    extractor.read_rds_table(engine, table)
