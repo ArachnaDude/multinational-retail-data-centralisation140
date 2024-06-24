8cfd6e5be3234d8f1068a67d935417d2af5f27f0
Matt <cunning.distraction@googlemail.com>
2024-06-24 23:51:28 +0100

    remove unnecessary imports from file

--- data_extraction.py
+++ data_extraction.py
@@ -8,16 +8,15 @@ class DataExtractor():
   def read_rds_table(self, engine, table_name="legacy_users"):
     engine.connect()
     df = pd.read_sql_table(table_name, engine)
-    # print(type(df))
     return df
   
 
 if __name__ == "__main__":
   connection = DatabaseConnector()
-  engine = connection.init_db_engine()
-  table_list = connection.list_db_tables()
+  # engine = connection.init_db_engine("./db_creds.yaml")
+  table_list = connection.list_db_tables("./db_creds.yaml")
 
-  extractor = DataExtractor()
+  # extractor = DataExtractor()
 
-  for table in table_list:
-    extractor.read_rds_table(engine, table)
+  # for table in table_list:
+  #   extractor.read_rds_table(engine, table)
