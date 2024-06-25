597f25773b05ce9018f284bf2cc3eec9f6050f41
Matt <cunning.distraction@googlemail.com>
2024-06-25 02:22:28 +0100

    remove class imports

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,17 +1,5 @@
-from data_extraction import DataExtractor
-from database_utils import DatabaseConnector
-
 import pandas as pd
 from datetime import datetime
-
-connection = DatabaseConnector()
-engine = connection.init_db_engine()
-
-extractor = DataExtractor()
-legacy_users_df = extractor.read_rds_table(engine, "legacy_users")
-legacy_store_df = extractor.read_rds_table(engine, "legacy_store_details")
-orders_df = extractor.read_rds_table(engine, "orders_table")
-card_details_df = extractor.read_rds_table(engine, "dim_card_details")
 
 
 class DataCleaning():
