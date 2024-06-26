3301ac4f7b2f9949a6d670e3d6861da293bdb4d6
Matt <cunning.distraction@googlemail.com>
2024-06-26 02:54:32 +0100

    add retrieve_pdf_data function

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,8 @@
 from database_utils import DatabaseConnector
 import pandas as pd
+import tabula
+
+import time
 
 
 class DataExtractor():
@@ -10,13 +13,30 @@ class DataExtractor():
     df = pd.read_sql_table(table_name, engine)
     return df
   
+  def retrieve_pdf_data(self, link_to_pdf):
+    print("Beginning PDF read")
+    start_time = time.time()
+    #returns a list of len 1, the 0th element is the dataframe containing the entire pdf data
+    dfs = tabula.read_pdf(link_to_pdf, pages="all", stream=True, multiple_tables=False)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    # print(f"Execution time: {elapsed_time:.2f} seconds")
+    # return the dataframe
+    dataframe = dfs[0]
+    print("Read complete - loading dataframe")
+    return dataframe
+  
+
+
+  
 
 if __name__ == "__main__":
-  connection = DatabaseConnector()
-  # engine = connection.init_db_engine("./db_creds.yaml")
-  table_list = connection.list_db_tables("./db_creds.yaml")
+  # connection = DatabaseConnector()
 
-  # extractor = DataExtractor()
+  # table_list = connection.list_db_tables("./db_creds.yaml")
 
-  # for table in table_list:
-  #   extractor.read_rds_table(engine, table)
+  link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+
+  extractor = DataExtractor()
+  extractor.retrieve_pdf_data(link)
+
