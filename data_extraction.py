9098875c83cf8ac91415aff4e92e552c2b65cb42
Matt <cunning.distraction@googlemail.com>
2024-06-27 04:36:29 +0100

    refactor extraction to accomodate yaml cred loading

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,7 @@
 from database_utils import DatabaseConnector
 import pandas as pd
 import tabula
-
+import yaml
 import time
 
 
@@ -13,17 +13,24 @@ class DataExtractor():
     df = pd.read_sql_table(table_name, engine)
     return df
   
-  def retrieve_pdf_data(self, link_to_pdf):
+
+  def retrieve_pdf_data(self, pdf_path):
+
+    with open (pdf_path, "r") as stream:
+      config = yaml.safe_load(stream)
+    link = config["LINK"]
+
     print("Beginning PDF read")
     start_time = time.time()
     #returns a list of len 1, the 0th element is the dataframe containing the entire pdf data
-    dfs = tabula.read_pdf(link_to_pdf, pages="all", stream=True, multiple_tables=False)
+    dfs = tabula.read_pdf(link, pages="all", stream=True, multiple_tables=False)
     end_time = time.time()
     elapsed_time = end_time - start_time
     # print(f"Execution time: {elapsed_time:.2f} seconds")
     # return the dataframe
     dataframe = dfs[0]
     print("Read complete - loading dataframe")
+    
     return dataframe
   
 
@@ -35,8 +42,8 @@ if __name__ == "__main__":
 
   # table_list = connection.list_db_tables("./db_creds.yaml")
 
-  link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+  path = "./pdf_link.yaml"
 
   extractor = DataExtractor()
-  extractor.retrieve_pdf_data(link)
+  extractor.retrieve_pdf_data(path)
 
