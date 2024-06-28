80dd78db6cad868fac645feb7b80f064108515f7
Matt <cunning.distraction@googlemail.com>
2024-06-28 17:07:57 +0100

    refactor list number and retrieve data into two separate function

--- data_extraction.py
+++ data_extraction.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tabula
 import yaml
 import time
+import requests
 
 
 class DataExtractor():
@@ -20,7 +21,7 @@ class DataExtractor():
       config = yaml.safe_load(stream)
     link = config["LINK"]
 
-    print("Beginning PDF read")
+    print("Beginning PDF read\nOperation takes approx. 130s")
     start_time = time.time()
     #returns a list of len 1, the 0th element is the dataframe containing the entire pdf data
     dfs = tabula.read_pdf(link, pages="all", stream=True, multiple_tables=False)
@@ -34,16 +35,66 @@ class DataExtractor():
     return dataframe
   
 
+  def list_number_of_stores(self, api_creds):
 
+    with open (api_creds, "r") as stream:
+      creds = yaml.safe_load(stream)
+    
+    try:
+      r = requests.get(creds["num_of_stores"], headers={"x-api-key": creds["x-api-key"]})
+      r.raise_for_status()
+      print(f"status code: {r.status_code}")
+    except requests.RequestException as e:
+      print(f"Error: {e}")
+      return
+
+    number = r.json()["number_stores"]
+    print(f"Data to be pulled from {number} stores")
+
+    return number
   
 
+  def retrieve_stores_data(self, api_creds):
+
+    with open (api_creds, "r") as stream:
+      creds = yaml.safe_load(stream)
+
+    store_path = creds["store_path"]
+
+    total_stores = self.list_number_of_stores(api_creds)
+
+    all_stores_data = []
+
+    start_time = time.time()
+    for store_number in range(total_stores):
+      url = store_path.format(store_number)
+
+      try:
+        r = requests.get(url, headers={"x-api-key": creds["x-api-key"]})
+        r.raise_for_status()
+        store_data = r.json()
+        all_stores_data.append(store_data)
+
+      except requests.RequestException as e:
+        print(f"Error: {e}")
+        return
+      
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Retrieved data in {execution_time:.2f} seconds")
+
+    all_stores_data = pd.DataFrame.from_dict(all_stores_data)
+    return all_stores_data
+
 if __name__ == "__main__":
+  path = "./pdf_link.yaml"
+  api_creds = "./api_creds.yaml"
   # connection = DatabaseConnector()
+  extractor = DataExtractor()
 
   # table_list = connection.list_db_tables("./db_creds.yaml")
 
-  path = "./pdf_link.yaml"
 
-  extractor = DataExtractor()
-  extractor.retrieve_pdf_data(path)
+  # extractor.retrieve_pdf_data(path)
 
+  extractor.retrieve_stores_data(api_creds)
